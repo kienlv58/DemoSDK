3aa6817a454b7a97759d0641f7db57de0a8c1849
Lò Văn Kiên <kien.lovan@nccsoft.vn>
2017-11-20 11:31:20 +0700

    test call

--- app/src/main/java/vn/demo/com/demosdk/MainActivity.java
+++ app/src/main/java/vn/demo/com/demosdk/MainActivity.java
@@ -3,11 +3,15 @@ package vn.demo.com.demosdk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import vn.demo.com.sdklibrary.callapi.TestCall1;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TestCall1.call1();
     }
 }
